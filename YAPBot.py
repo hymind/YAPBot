93d11edcf155eacb096201de6c2044bea3cbba65
hymind <shanibaram@gmail.com>
2025-05-25 00:20:37 +0300

    Add manual updating
    v0.1.2
    This will probably make upscaling a metric fuckton easier

--- YAPBot.py
+++ YAPBot.py
@@ -41,6 +41,62 @@ aliases = {'gless': 'glitchless',
            'inb': 'inbounds',
            'oob': 'out of bounds',
            'unr': 'unrestricted'}
+import discord
+from discord.ext import commands
+from math import floor
+import json
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'Logged in as {bot.user}')
+def fixticks(sometime):
+    ticks = sometime * (200/3)
+    ticks = round(ticks)
+    sometime = ticks / (200/3)
+    return sometime
+def mmss_to_s(time_str):
+    minutes, seconds = time_str.split(':')
+    time_str =  int(minutes) * 60 + float(seconds)
+    time_str = fixticks(time_str)
+    return time_str
+def time_to_mmss(sometime):
+    minutes = floor(sometime / 60)
+    seconds = sometime % 60
+    return f"{minutes}:{seconds:06.3f}"
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+allowedCategories = ['glitchless', 'inbounds', 'out of bounds', 'legacy', 'unrestricted', 'isg']
+aliases = {'gless': 'glitchless',
+           'gl': 'glitchless',
+           'inb': 'inbounds',
+           'oob': 'out of bounds',
+           'unr': 'unrestricted'}
+def submitpb_manual(category, sometime): # For internal use only, inaccessible to users. Assumes sanitized data.
+    if is_number(sometime):
+        time = fixticks(float(sometime))
+    else:
+        time = mmss_to_s(sometime)
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        if user not in data:
+            data[user] = {}
+            data[user][category] = time
+        with open('data.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        return
 @bot.hybrid_command()
 async def submitpb(ctx, category: str, time: str):
     category = category.lower()
