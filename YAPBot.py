654f5ef69444638dc7893ecc9cc4bb84822ddc3b
hymind <shanibaram@gmail.com>
2025-05-24 22:20:44 +0300

    Minor bugfix
    Added error handling & edgecase support

--- YAPBot.py
+++ YAPBot.py
@@ -24,14 +24,57 @@ def mmss_to_s(time_str):
 def time_to_mmss(sometime):
     minutes = floor(sometime / 60)
     seconds = sometime % 60
-    return f"{minutes}:{seconds:.3f}"
+    return f"{minutes}:{seconds:06.3f}"
 def is_number(s):
     try:
         float(s)
         return True
     except ValueError:
         return False
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
 
+allowedCategories = ['glitchless', 'inbounds', 'out of bounds', 'legacy', 'unrestricted', 'isg']
+aliases = {'gless': 'glitchless',
+           'gl': 'glitchless',
+           'inb': 'inbounds',
+           'oob': 'out of bounds',
+           'unr': 'unrestricted'}
+import discord
+from discord.ext import commands
+from math import floor
+import json
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'Logged in as {bot.user}')
+def fixticks(sometime):
+    ticks = sometime * (200/3)
+    ticks = round(ticks)
+    sometime = ticks / (200/3)
+    return sometime
+def mmss_to_s(time_str):
+    minutes, seconds = time_str.split(':')
+    time_str =  int(minutes) * 60 + float(seconds)
+    time_str = fixticks(time_str)
+    return time_str
+def time_to_mmss(sometime):
+    minutes = floor(sometime / 60)
+    seconds = sometime % 60
+    return f"{minutes}:{seconds:06.3f}"
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+@bot.event
 async def on_message(message):
     await bot.process_commands(message)
 
@@ -43,6 +86,7 @@ aliases = {'gless': 'glitchless',
            'unr': 'unrestricted'}
 @bot.hybrid_command()
 async def submitpb(ctx, category: str, time: str):
+    category = category.lower()
     if category not in allowedCategories:
         if category in aliases:
             category = aliases[category]
@@ -73,7 +117,11 @@ async def pf(ctx, user: str):
     output = f"{user}'s PBs:"
     with open('data.json', 'r') as f:
         data = json.load(f)
-        for cat in data[user]:
-            output += f"\n {cat}: {time_to_mmss(data[user][cat])}"
+        try:
+            for cat in data[user]:
+                output += f"\n {cat}: {time_to_mmss(data[user][cat])}"
+        except KeyError:
+            await ctx.send("User not found in database! Make sure you spelled their name correctly.")
+            return
     await ctx.send(output)
     return
